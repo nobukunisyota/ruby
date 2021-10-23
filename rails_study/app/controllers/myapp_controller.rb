18c0d64ec20855f70b9ec2262e099f009b3c5a8c
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-10-23 12:59:16 +0900

    rename rails_study to rails_tutorial

--- rails_study/app/controllers/myapp_controller.rb
+++
@@ -1,5 +0,0 @@
-class MyappController < ApplicationController
-    def index
-        
-    end
-end
--- rails_study/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :members
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'myapp#index'
-end
